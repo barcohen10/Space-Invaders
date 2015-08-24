95a33f30e87987e5d6d7cf51287a6a325febccb2
barcohen10 <barcohen10@gmail.com>
2015-08-24 22:49:24 +0300

    new CenterTextsOnScreen Method
    Signed-off-by: barcohen10 <barcohen10@gmail.com>

--- SpaceInvaders/Services/TextServices.cs
+++ SpaceInvaders/Services/TextServices.cs
@@ -11,19 +11,42 @@ namespace C15Ex02Dotan301810610Bar308000322.Services
 {
     public static class TextServices
     {
-        public static void CreateAndAdjustTexts(GameScreen i_GameScreen,List<string> i_Texts,SpritesFactory.eSpriteType i_SpriteType,float i_PositionX,float i_StartPositionY)
+        public static List<Text> GetAndCreateTexts(GameScreen i_GameScreen, List<string> i_Texts, SpritesFactory.eSpriteType i_SpriteType, float i_PositionX, float i_PositionY)
         {
             Text spriteText = null;
-            float dynamicPositionY = i_StartPositionY;
+            List<Text> texts = new List<Text>();
+            float dynamicPositionY = i_PositionY;
             foreach (string text in i_Texts)
             {
-               spriteText =  SpritesFactory.CreateSprite(i_GameScreen, i_SpriteType) as Text;
+               spriteText = SpritesFactory.CreateSprite(i_GameScreen, i_SpriteType) as Text;
                 if (spriteText != null)
                 {
-                    spriteText.Position =new Vector2(i_PositionX, dynamicPositionY);
+                    spriteText.Position = new Vector2(i_PositionX, dynamicPositionY);
                     spriteText.TextString = text;
                     dynamicPositionY += spriteText.Height;
                 }
+                texts.Add(spriteText);
+            }
+
+            return texts;
+        }
+
+        public static void CenterTextsOnScreen(GameScreen i_GameScreen, List<Text> i_Texts)
+        {
+            float x = (float)i_GameScreen.GraphicsDevice.Viewport.Width / 2;
+            float largestTextWidth = 0;
+            foreach(Text text in i_Texts)
+            {
+                if(largestTextWidth < text.Width)
+                {
+                    largestTextWidth = text.Width;
+                }
+            }
+
+            x -= largestTextWidth / 2;
+            foreach (Text text in i_Texts)
+            {
+                text.Position = new Vector2(x, text.Position.Y);
             }
         }
     }
