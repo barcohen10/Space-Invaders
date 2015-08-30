cbcb1f771ca1ab51ebcde39cab46d24ee7c76345
ddotan <ddotand@gmail.com>
2015-08-30 16:06:24 +0300

    .

--- SpaceInvaders/Menu/ConcreteMenuItems/TextMenuItem.cs
+++ SpaceInvaders/Menu/ConcreteMenuItems/TextMenuItem.cs
@@ -13,14 +13,10 @@ namespace C15Ex03Dotan301810610Bar308000322.Menu
 {
     public class TextMenuItem : GameMenuItem
     {
-        private Sound m_MoveScreenSound;
 
         public TextMenuItem(string i_ItemName, GameScreen i_GameScreen, params MethodKey[] i_Methods)
             : base(i_ItemName, i_GameScreen, Color.White, GameMenuItem.eFontSize.Medium, i_Methods)
         {
-            m_MoveScreenSound = SoundFactory.CreateSound(this.m_GameScreen.Game, SoundFactory.eSoundType.MenuMove);
-        //To Play It :
-            m_MoveScreenSound.Play();
         }
     }
 }
